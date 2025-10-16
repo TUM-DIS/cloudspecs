59962c26b923ffaddb19a4e07a91a7ed4d1c78fd
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-10-16 09:51:01 +0200

    Use colors from paper, provide vendor palette

--- components/RRepl.js
+++ components/RRepl.js
@@ -27,6 +27,25 @@ export default class RRepl {
     }
   }
 
+    static #rPrelude() {
+       return `
+library(svglite);
+library(ggplot2)
+colors.area <- c(green = "#a3be8c", "dark-green" = "#469800", purple = "#b48ead", yellow = "#ebcb8b", frost = "#8fbcbb", "dark-blue" = "#5e81ac", "medium-grey" = "#4c566a", red = "#bf616a", "dark-red" = "#ab1b00", orange = "#ff9900", "light-blue" = "#81a1c1", "white-grey" = "#d8dee9", ice = "#88c0d0", carbon = "#2e3440", black = "black")
+colors.disc <- c(green = "#8DBE64", "dark-green" = "#469800", purple = "#B470A7", yellow = "#EBB13E", frost = "#6BBCBA", "dark-blue" = "#205FAC", "medium-grey" = "#34466A", red = "#BF1626", "dark-red" = "#AB1B00", orange = "#FF9900", "light-blue" = "#4E87C1", "white-grey" = "#CAD5E9", ice = "#4EB3D0", carbon = "#202A40", black = "#000000")
+make.palette <- function(input_mapping, colors_map = colors.disc) {
+  # Get the values (colors) from input_mapping and replace with hex values from colors_map
+  hex_values <- colors_map[unname(input_mapping)]
+  # Return the vector with column names mapped to hex values
+  output_vector <- setNames(hex_values, names(input_mapping))
+  return(output_vector)
+}
+colormap.vendor <- c('Unknown' = 'grey', 'AMD' = 'carbon', 'Intel' = 'dark-blue', 'Graviton' = 'orange', 'AWS Graviton' = 'orange')
+palette.vendor <- make.palette(colormap.vendor)
+options(ggplot2.discrete.colour= unname(colors.disc[c("dark-green", "dark-blue", "dark-red", "purple", "yellow", "carbon", "orange", "light-blue", "ice")]))
+` 
+    }
+
   minimalRCode() {
     return `to_svg <- svgstring(width = output.width.inch, height = output.height.inch, scaling = 1)
 theme_set(theme_bw())
@@ -45,7 +64,7 @@ plot(output); dev.off(); to_svg()`
     const webR = new WebR();
     await webR.init();
     await webR.installPackages(['ggplot2', 'svglite'], { quiet: true, mount: true });
-    await webR.evalRVoid("library(svglite); library(ggplot2)");
+    await webR.evalRVoid(this.#rPrelude());
     return webR;
   }
 
